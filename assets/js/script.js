4a052ba54ffc57cb9c20a90c24ed9e937bc28af9
abdohantash <62960904+abdohantash@users.noreply.github.com>
2021-11-10 14:52:30 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -235,18 +235,6 @@ jQuery(document).on('ready', function ($) {
         }
     });
 
-    /*----------------------------------
-    //----- JQUERY COUNTER UP -----//
-    -----------------------------------*/
-    $('.counter').counterUp({
-        delay: 10,
-        time: 5000,
-        offset: 100,
-        beginAt: 0,
-        formatter: function (n) {
-            return n.replace(/,/g, '.');
-        }
-    });
 
     /*----------------------------------
     //------ MAGNIFIC POPUP ------//
